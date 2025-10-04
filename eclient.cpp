c2aa71e87e2814f87f438c0bdb98af604fb88e00
aspirin100 <aspi.jb.ru@gmail.com>
2025-10-04 15:01:49 +0300

    refactoring

--- eclient.cpp
+++ eclient.cpp
@@ -4,38 +4,52 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <memory>
+#include <string>
 
-int main(int argc, char** argv){
-	if(argc != 2){
-		std::cout << "wrong arg count\n";
-		return -1;	
-	}
-
+int main(){	
 	std::unique_ptr<sockaddr_in> saddr{new sockaddr_in};
-	int sock;
 	
-	if(sock = socket(AF_INET, SOCK_STREAM, 0); sock < 0){
+	auto sock_closer = [](int *sock_ptr){
+		if(sock_ptr)
+			close(*sock_ptr);
+		
+		delete sock_ptr;
+	};
+
+	std::unique_ptr<int, decltype(sock_closer)> sock(new int, sock_closer);
+	
+	if(*sock = socket(AF_INET, SOCK_STREAM, 0); *sock < 0){
 		perror("socket");
 		return -1;
 	}
 
+	short port;
+
+	std::cout << "enter server port:\n";
+	std::cin >> port;
+
 	saddr->sin_family = AF_INET;
-	saddr->sin_port = htons(8080);
+	saddr->sin_port = htons(port);
 	saddr->sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-	if(connect(sock, (sockaddr*)&(*saddr), sizeof(*saddr)) < 0){
+	if(connect(*sock, (sockaddr*)&(*saddr), sizeof(*saddr)) < 0){
 		perror("connect");
 		return -1;
 	}
 
-	char buf[sizeof(argv[1])];
+	std::string msg, received_msg;
+	while(true){
+		std::cin >> msg;
+		received_msg.resize(msg.size());
 
-	send(sock, argv[1], sizeof(argv[1]), 0);
-	recv(sock, buf, sizeof(argv[1]), 0);
+		if(send(*sock, msg.data(), msg.size(), 0) < 0){
+			perror("send");
+			continue;
+		}
 
-	std::cout << buf << "\n";
-
-	close(sock);
+		recv(*sock, received_msg.data(), msg.size(), 0);
+		std::cout << "server: " << received_msg << "\n";
+	}
 
 	return 0;
 }
--- eserver.cpp
+++ eserver.cpp
@@ -7,9 +7,15 @@
 
 int main(){
 	std::unique_ptr<sockaddr_in> saddr{new sockaddr_in};
-	int listener_sock;
+	auto socket_closer = [](int *sock_ptr){
+		if(sock_ptr)
+			close(*sock_ptr);
+		delete(sock_ptr);
+	};
+	
+	std::unique_ptr<int, decltype(socket_closer)> listener_sock(new int, socket_closer);
 
-	if(listener_sock = socket(AF_INET, SOCK_STREAM, 0); listener_sock < 0){
+	if(*listener_sock = socket(AF_INET, SOCK_STREAM, 0); *listener_sock < 0){
 		perror("socket");
 		return -1;
 	}
@@ -23,23 +29,27 @@ int main(){
 	saddr->sin_port = htons(port);
 	saddr->sin_addr.s_addr = INADDR_ANY; 
 
-	if(bind(listener_sock, (sockaddr*)&(*saddr), sizeof(*saddr)) < 0){
+	if(bind(*listener_sock, (sockaddr*)&(*saddr), sizeof(*saddr)) < 0){
 		perror("bind");
 		return -1;
 	}
 
-	listen(listener_sock, 1);
+	listen(*listener_sock, 1);
 
 	while(true){
-		int sock = accept(listener_sock, nullptr, nullptr);
+		std::unique_ptr<int, decltype(socket_closer)> client_fd(new int, socket_closer);
+		if(*client_fd = accept(*listener_sock, nullptr, nullptr); *client_fd < 0){
+			perror("accept");
+			return -1;
+		}
+		
 		char buff[1024];
 		while(true){
-			int readed = recv(sock, buff, 1024, 0);
+			int readed = recv(*client_fd, buff, 1024, 0);
 
 			if (readed <= 0) break;
-			send(sock, buff, readed, 0);
+			send(*client_fd, buff, readed, 0);
 		}
-		close(sock);
 	}
 
 	return 0;
